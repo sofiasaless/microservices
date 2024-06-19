06007fa556c82e66dcd8bdd48788e99c6c5a2e8b
sofiasales02 <sofiasaleswk@gmail.com>
2024-06-18 21:29:14 -0300

    commit inicial

---
+++ core/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.learningMs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
